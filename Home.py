1f44372958584abc948f07b52f97ac446b9828d6
Vivien <samanthaservo09@gmail.com>
2024-04-04 16:52:57 +0800

    Added adjustments with the pages and their initial contents

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st  
 
 st.set_page_config(
-    page_title="My Webpage",
+    page_title="Samantha Servo's Portfolio",
     page_icon="👾",
 )
 
-st.title("Main Page")
+st.title("Beyond the Vernaculum")
 st.sidebar.success("Select a page above.")
 
---
+++ pages/resume.py
@@ -0,0 +1,3 @@
+import streamlit as st 
+
+st.title("Here's my digital resume")
