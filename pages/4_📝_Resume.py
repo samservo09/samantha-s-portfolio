80dd561b4e43f28ed1c1fab8cc543be9de34e4e6
Vivien <samanthaservo09@gmail.com>
2024-04-13 22:08:03 +0800

    added contents to the hero section of resume

--- pages/4_📝_Resume.py
+++ pages/4_📝_Resume.py
@@ -1,3 +1,26 @@
 import streamlit as st 
+from PIL import Image
 
-st.title("Here's my digital resume")
+resume = "resume.pdf"
+profile = "img\profile-pic.png"
+
+with open(resume, "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+profile_pic = Image.open(profile)
+
+#hero section
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image(profile, width=230)
+
+with col2:
+    st.title("SAMANTHA VIVIEN L. SERVO")
+    st.write("Aspiring Data Scientist, advocate of data-driven business decison-making.")
+    st.download_button(
+        label="🗎 Download Resume",
+        file_name='samantha-servo-resume.pdf',
+        data=PDFbyte,
+        mime="application/octet-stream"
+    )
+    st.write("📧: samanthaservo09@gmail.com")
+    
