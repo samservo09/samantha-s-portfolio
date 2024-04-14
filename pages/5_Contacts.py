00164812b4052d0ad4a35b4cfc0fa50ef4013973
Vivien <samanthaservo09@gmail.com>
2024-04-14 17:32:47 +0800

    added contents to the contact form

---
+++ pages/5_Contacts.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+
+st.header(":mailbox: Get In Touch With Me!")
+
+#link for connecting my form to formsubmit's endpoint
+contact_form = """
+<form action="https://formsubmit.co/samanthaservo09@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Your name" required>
+    <input type="email" name="email" placeholder="Your email" required>
+    <textarea name="message" placeholder="Your message here"></textarea>
+    <button type="submit">Send</button>
+</form>
+"""
+
+#inject the HTML code into my website
+st.markdown(contact_form, unsafe_allow_html=True)
+
+
+
