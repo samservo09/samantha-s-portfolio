550e65904788a3c14d27e36674300986a8b08622
Vivien <samanthaservo09@gmail.com>
2024-04-14 17:57:22 +0800

    added placeholder contents to the project page

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -1,3 +1,14 @@
 import streamlit as st 
 
 st.title("Welcome to my projects!")
+
+st.markdown("*Note: Everything here is still a placeholder. I will soon add my personal projects!*")
+
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.image("img\project1.jpg", width=230)
+
+with col2:
+    st.subheader("Project 1")
+    st.write("Project description here.")
+    st.markdown("[Visit Github Repo](https://github.com/samservo09/samantha-s-portfolio.git)")
