acd79cf8c4e1ddde385291250bd297aa00a9babb
Vivien <samanthaservo09@gmail.com>
2024-05-29 18:01:45 +0800

    adjusted the image url in projects

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -1,12 +1,20 @@
 import streamlit as st 
+from pathlib import Path
 
 st.title("Welcome to my projects!")
 
 st.markdown("*Note: Everything here is still a placeholder. I will soon add my personal projects!*")
 
+#define image path
+cwd = Path.cwd()
+img_dir = cwd / 'img'
+image_path2 = img_dir / 'project1.jpg'
+
 col1, col2 = st.columns(2, gap="small")
 with col1:
-    st.image("img\project1.jpg", width=230)
+    #insert my profile picture
+    if image_path2.exists() and image_path2.is_file():
+        st.image(str(image_path2), width=230)  # Convert Path object to string
 
 with col2:
     st.subheader("Project 1")
