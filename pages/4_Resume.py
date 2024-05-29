a89f641690dfac47eb0dea03be7da644f0f657e9
Vivien <samanthaservo09@gmail.com>
2024-05-29 18:07:09 +0800

    adjusted image file in resume page

--- pages/4_Resume.py
+++ pages/4_Resume.py
@@ -1,17 +1,21 @@
 import streamlit as st 
 from PIL import Image
+from pathlib import Path
 
 resume = "resume.pdf"
-profile = "img\profile-pic.png"
 
 with open(resume, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile)
+
+#define image path
+cwd = Path.cwd()
+img_dir = cwd / 'img'
+image_path = img_dir / 'profile-pic.png'
 
 #hero section
 col1, col2 = st.columns(2, gap="small")
 with col1:
-    st.image(profile, width=230)
+    st.image(image_path, width=230)
 
 with col2:
     st.title("SAMANTHA VIVIEN L. SERVO")
