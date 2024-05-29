ea516e18e21647949c835672b1a4f97ab88f1760
Vivien <samanthaservo09@gmail.com>
2024-05-29 17:49:45 +0800

    adjusted the image path

--- Home.py
+++ Home.py
@@ -8,13 +8,19 @@ st.set_page_config(
 
 st.sidebar.success("Select a page above.")
 
-st.markdown("🚧 *Note: This website portoflio is underconstruction.* 🚧")
+st.markdown("🚧 *Note: This website portfolio is under construction.* 🚧")
 
 #define image path
 cwd = Path.cwd()
 img_dir = cwd / 'img'
 image_path = img_dir / 'profile-pic.png'
 st.write(f"Image path: {image_path}")
+
+# Check if the file exists
+if image_path.exists() and image_path.is_file():
+    st.write("Image file found.")
+else:
+    st.error(f"Image file not found: {image_path}")
 
 #division of two different contents
 col1, col2 = st.columns(2, gap="small")
@@ -24,7 +30,8 @@ with col1:
 
 with col2:
     #insert my profile picture
-    st.image(img_dir, width=230)
+    if image_path.exists() and image_path.is_file():
+        st.image(str(image_path), width=230)  # Convert Path object to string
 
 #short description about me and my site
 st.write("I am an aspiring Data Scientist.")
