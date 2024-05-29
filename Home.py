207333f70130a5a6f62161607ad90da0c8db2918
Vivien <samanthaservo09@gmail.com>
2024-05-29 17:41:32 +0800

    adjusted img path in home

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st  
+from pathlib import Path
 
 st.set_page_config(
     page_title="Samantha Servo's Portfolio",
@@ -8,6 +9,12 @@ st.set_page_config(
 st.sidebar.success("Select a page above.")
 
 st.markdown("🚧 *Note: This website portoflio is underconstruction.* 🚧")
+
+#define image path
+cwd = Path.cwd()
+img_dir = cwd / 'img'
+image_path = img_dir / 'profile-pic.png'
+st.write(f"Image path: {image_path}")
 
 #division of two different contents
 col1, col2 = st.columns(2, gap="small")
