c92ad0071820b4071fde23923cf4dec34dac5d4c
Vivien <samanthaservo09@gmail.com>
2024-05-29 17:59:19 +0800

    adjusted about page image url

--- pages/2_About.py
+++ pages/2_About.py
@@ -1,4 +1,5 @@
 import streamlit as st  
+from pathlib import Path
 
 st.set_page_config(
     page_title="Samantha Servo's Portfolio",
@@ -6,6 +7,11 @@ st.set_page_config(
 )
 
 st.sidebar.success("Select a page above.")
+
+#define image path
+cwd = Path.cwd()
+img_dir = cwd / 'img'
+image_path = img_dir / 'profile-pic.png'
 
 #division of two different contents
 col1, col2 = st.columns(2, gap="small")
@@ -15,7 +21,8 @@ with col1:
 
 with col2:
     #insert my profile picture
-    st.image("img\profile-pic.png", width=230)
+    if image_path.exists() and image_path.is_file():
+        st.image(str(image_path), width=230)  # Convert Path object to string
 
 #short description about me and my site
 st.write("Welcome to my portfolio website! This is a website I have created to compile and showcase all information about me. That includes my projects so you can see how we can work together in the future!")
