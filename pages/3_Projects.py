70e037aac3d18ac8b08919d69884cbe5de2f6304
Vivien <samanthaservo09@gmail.com>
2024-06-02 11:25:27 +0800

    added new changes in project page

--- pages/3_Projects.py
+++ pages/3_Projects.py
@@ -27,12 +27,28 @@ with col2:
     
 
 st.subheader("Python Projects")
-st.markdown("soon to be added")
+#define image path
+cwd = Path.cwd()
+img_dir = cwd / 'img'
+image_path2 = img_dir / 'project1 .png'
+
+col1, col2 = st.columns(2, gap="small")
+
+with col1:
+    #insert project picture
+    if image_path2.exists() and image_path2.is_file():
+        st.image(str(image_path2), width=350)  # Convert Path object to string
+
+with col2:
+    st.subheader("Stoicsim Quote Generator: Send a daily quote to your email!")
+    st.write("I have used a stoic API to generate the random quotes. You can try and start receiving a random stoic quote! Note: This project is still in progress.")
+    st.markdown("[View my writeup about this project](https://docs.google.com/document/d/1qAS0TvFBrZcxB97Wa6A9cP0m5uilu7tOH7y8FE5d6og/edit?usp=sharing)")
+    st.markdown("[Visit Project](https://github.com/samservo09/stoic-quote-generator.git)")
 
 st.subheader("SQL Projects")
 st.markdown("soon to be added")
 
-st.subheader("PowerBI Projects")
+st.subheader("Data Visualization Projects")
 st.markdown("soon to be added")
 
 st.subheader("End-to-end Data Science Project")
